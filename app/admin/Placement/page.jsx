47455ee7e6d20b937eaca2fa95e136eace9e2d8b
hashiq <mohdhashique10@gmail.com>
2023-10-12 00:43:36 +0530

    placement

--- app/admin/Placement/page.jsx
+++ app/admin/Placement/page.jsx
@@ -1,62 +1,64 @@
 "use client";
 
 import styles from "@styles/adminPage.module.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
-import FacultyInput from "@components/admin/FacultyInput";
 import getDetailsFromFirebase from "@util/getDetailsFromFirebase";
-import { setFaculties } from "@app/redux/features/faculty/facultySlice";
 import AdminDeleteButton from "@components/admin/AdminDeleteButton";
-import { deleteFaculty } from "@app/redux/features/faculty/facultySlice";
+
+import PlacementInput from "@components/admin/PlacementInput";
+import {
+  deletePlacement,
+  setPlacements,
+} from "@app/redux/features/placement/placementSlice";
 
 const Placement = () => {
   const dispatch = useDispatch();
-  const [companies, setCompanies] = useState([]);
+  const placements = useSelector((state) => state.placement.placements);
 
   useEffect(() => {
     const getDetails = async () => {
-      const faculties = await getDetailsFromFirebase("placements");
-      const Firebasecompanies = await getDetailsFromFirebase("companies");
-      setCompanies(Firebasecompanies)
-      dispatch(setFaculties(faculties));
+      const placementsData = await getDetailsFromFirebase("placements");
+      dispatch(setPlacements(placementsData));
     };
     getDetails();
   }, []);
 
-  const faculties = useSelector((state) => state.faculty.faculties);
-
   return (
     <div className={`row`}>
       <div className={`col-lg-6`}>
-        <FacultyInput />
+        <PlacementInput />
       </div>
       <div className={`col-lg-6`}>
         <div className={styles.admin_output_section}>
-          {faculties.length > 0 ? (
-            faculties.map((d) => {
-              console.log(d);
-              return (
-                <div key={d.id} className={styles.facility_output_card}>
-                  <div className={styles.card_content}>
-                    <h3>{d.facultyName}</h3>
-                    <AdminDeleteButton
-                      id={d.id}
-                      collection="faculties"
-                      dispatchFunction={deleteFaculty}
-                    />
-                  </div>
-                  <hr />
+          {placements.length > 0 ? (
+            placements.map((d) => (
+              <div key={d.id} className={styles.facility_output_card}>
+                <div className={styles.card_content}>
+                  <h3>
+                    {d.name}&nbsp;({d.year})
+                  </h3>
+                  <AdminDeleteButton
+                    id={d.id}
+                    collection="placements"
+                    dispatchFunction={deletePlacement}
+                  />
                 </div>
-              );
-            })
+                <hr />
+              </div>
+            ))
           ) : (
-            <p>Nothing To show</p>
+            <p className="text-center mt-3">Nothing To show</p>
           )}
         </div>
       </div>
     </div>
   );
 };
+
+
+
+
 
 export default Placement;
