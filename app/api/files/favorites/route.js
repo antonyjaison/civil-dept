6d8863f666c38a9f5f21b4be537bb60d8af45eb0
Antony Jaison <antonyjaison639@gmail.com>
2023-11-06 13:19:18 +0530

    fixed errors

--- app/api/files/favorites/route.js
+++ app/api/files/favorites/route.js
@@ -5,13 +5,13 @@ export async function GET(request) {
     const q = query(collection(db, 'favorites'), orderBy('created', 'desc'));
 
     try {
-        // const querySnapshot = await getDocs(q);
-        // const tasks = querySnapshot.docs.map(doc => ({
-        //     id: doc.id,
-        //     data: doc.data()
-        // }));
+        const querySnapshot = await getDocs(q);
+        const tasks = querySnapshot.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data()
+        }));
         
-        // return { body: JSON.stringify(tasks) };
+        return { body: JSON.stringify(tasks) };
     } catch (error) {
         console.error("Error fetching data:", error);
         return { status: 500, body: "Internal Server Error" };
--- components/FacilityCard.jsx
+++ components/FacilityCard.jsx
@@ -4,7 +4,7 @@ const FacilityCard = ({ heading, text, image}) => {
   return (
     <div className={`row`}>
       <div className={`col-lg-4 col-md-4`}>
-        <img className={`w-100`} src={image} alt="" />
+        <img className={`w-100`} src={image} alt="image" />
       </div>
       <div className={`col-lg-8 col-md-8 ${styles.content}`}>
         <h2>{heading}</h2>
