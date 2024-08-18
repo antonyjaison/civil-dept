824aa816f013e08e817806708477003bd6aa8eee
Amjedusman <amjadusman98@gmail.com>
2024-08-18 17:17:22 +0530

    frontend for Gallery admin added

--- app/admin/Gallery/page.jsx
+++ app/admin/Gallery/page.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+"use client";
 
-function page() {
+import styles from "@styles/adminPage.module.scss";
+import { useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import getDetailsFromFirebase from "@util/getDetailsFromFirebase";
+import { setFaculties } from "@app/redux/features/faculty/facultySlice";
+import AdminDeleteButton from "@components/admin/AdminDeleteButton";
+import { deleteFaculty } from "@app/redux/features/faculty/facultySlice";
+import GalleryInput from "@components/admin/GalleryInput";
+
+const Gallery = () => {
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   const getDetails = async () => {
+  //     const faculties = await getDetailsFromFirebase("faculties");
+  //     dispatch(setFaculties(faculties));
+  //   };
+  //   getDetails();
+  // }, []);
+
+  const faculties = useSelector((state) => state.faculty.faculties);
+
   return (
-    <div>Gallery Admin</div>
-  )
-}
+    <div className={`row`}>
+      <div className={`col-lg-6`}>
+        <GalleryInput />
+      </div>
+      <div className={`col-lg-6`}>
+        <div className={styles.admin_output_section}>
+          {faculties.length > 0 ? (
+            faculties.map((d) => {
+              return (
+                <div key={d.id} className={styles.facility_output_card}>
+                  <div className={styles.card_content}>
+                    <h3>{d.facultyName}</h3>
+                    <AdminDeleteButton
+                      id={d.id}
+                      collection="faculties"
+                      dispatchFunction={deleteFaculty}
+                    />
+                  </div>
+                  <hr />
+                </div>
+              );
+            })
+          ) : (
+            <p>Nothing To show</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default Gallery;
